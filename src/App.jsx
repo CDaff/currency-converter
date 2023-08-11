c90020bf5e5e8d792d43171c06fe2c2f88a84d21
CDaff <40277491+CDaff@users.noreply.github.com>
2023-08-11 06:57:09 -0500

    Added a default to both currency options. The 1st currency is the base currency from the JSON and the 2nd default is the 1st currency in the array.

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,17 @@ function App() {
 
   const [currencyOptions, setCurrencyOptions] = useState([])
   // console.log(currencyOptions) --testing currency option array was correctly pulled
+  const [fromCurrency, setFromCurrency] = useState([]);
+  const [toCurrency, setToCurrency] = useState([]);
 
   useEffect(() =>{
     fetch(BASE_URL)
       .then(res => res.json())
       .then(data => {
+        const firstCurrency = Object.keys(data.rates)[0]
         setCurrencyOptions([data.base, ...Object.keys(data.rates)])
+        setFromCurrency(data.base)
+        setToCurrency(firstCurrency)
       })
   }, [])
 
@@ -23,10 +28,12 @@ function App() {
       <h1>Currency Calculator: Convert</h1>
       <CurrencyRow
         currencyOptions={currencyOptions}
+        selectCurrency={fromCurrency}
       />
       <div className="equals">=</div>
       <CurrencyRow
         currencyOptions={currencyOptions}
+        selectCurrency={toCurrency}
       />
     </>
   );
